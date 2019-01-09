931107e59dbcaf7e6732a6d60cd9702e41702235
imstrive <longtaosu@sina.com>
2019-01-09 15:20:17 +0800

    项目初始化

--- DockerDeploy/Program.cs
+++ DockerDeploy/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace _01DockerDeploy
+namespace DockerDeploy
 {
     public class Program
     {
--- DockerDeploy/Startup.cs
+++ DockerDeploy/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace _01DockerDeploy
+namespace DockerDeploy
 {
     public class Startup
     {
